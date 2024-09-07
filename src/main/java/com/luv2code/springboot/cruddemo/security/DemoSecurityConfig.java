0e3698adcc544ab2ea92b0d1be79be99a86427d3
stefanosdeligiannis <stevedel98@gmail.com>
2024-09-08 02:08:16 +0300

    Spring Security-JDBC Custom Tables Setup

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -21,7 +21,14 @@ public class DemoSecurityConfig {
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource){
 
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        //define a query to retrieve user by userName
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id,pw,active from members where user_id=?");
+        //define a query to retrieve the authorities/roles by userName
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id,role from roles where user_id=?" );
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
